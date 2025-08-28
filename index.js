b1199c5cea931082e256069d0ceca227d4912ce5
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-28 14:16:56 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const multer = require("multer");
+const cors = require('cors');
+require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +12,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+const upload = multer({ dest: "./public/data/uploads/" });
+
+/**
+ * 
+ */
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  // req.file is the name of your file in the form above, here 'upfile'
+  // req.body will hold the text fields, if there were any
+  console.log(req.file, req.body);
 });
